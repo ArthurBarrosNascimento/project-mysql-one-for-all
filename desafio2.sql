cbf0d94fa182f9dd44aedeb534a93a9bfc3b32e4
Arthur de Barros <arthurbarros.nascimento12@gmail.com>
2023-01-16 17:28:11 -0300

    req dois feito

---
+++ desafio2.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(c.cacoes_id) AS cancoes,
+COUNT( DISTINCT art.artista_id) AS artistas,
+COUNT( DISTINCT alb.album_id) AS albuns
+FROM SpotifyClone.cacoes AS c
+INNER JOIN SpotifyClone.album AS alb ON c.album_id = alb.album_id
+INNER JOIN SpotifyClone.artista AS art ON alb.artista_id = art.artista_id;
